bd3ecb633431fa08f582364ceb08b801e8add2bd
Jose M Cuervo <josmansoft24@gmail.com>
2025-01-05 22:13:52 -0500

    first commit

--- src/db.js
+++ src/db.js
@@ -1,17 +1,11 @@
+// db.js
 import mysql from 'mysql2/promise';
-import {createPool} from 'mysql2';
-import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-} from './config.js';
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from './config.js'; // Asegúrate de la ruta correcta
 
 export const pool = mysql.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    port: DB_PORT
-})
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
+});
