b8eda9c23ce808b1cbf4a3cb935addef9bb66e79
Jose M Cuervo <josmansoft24@gmail.com>
2025-01-05 21:43:15 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+//import dotenv from 'dotenv';
+//dotenv.config();
 
 export const PORT = process.env.PORT || 3009
 
@@ -8,5 +8,5 @@ export const PORT = process.env.PORT || 3009
 export const DB_HOST = process.env.DB_HOST || 'localhost' //'192.168.1.9' //'192.168.56.1' // 
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '123'
-export const DB_NAME = process.env.DB_NAME || 'sarlaftt'
+export const DB_NAME = process.env.DB_NAME || 'sarlaft'
 export const DB_PORT = process.env.DB_PORT || '3306'
